53f627dc6d177e98bcaebede723b5ccd7650d6b6
Napppz <emailanda@example.com>
2026-04-10 03:12:19 +0700

    Tingkatkan pelaporan error dan penyederhanaan koneksi Neon

--- api/_db.js
+++ api/_db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('@neondatabase/serverless');
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_1yVLlBYH3qCM@ep-nameless-voice-ank083j1-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
+const connectionString = process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_1yVLlBYH3qCM@ep-nameless-voice-ank083j1-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require';
 
 const pool = new Pool({ connectionString });
 
