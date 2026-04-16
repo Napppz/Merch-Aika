b0d0f680aaebabedc1e70de3bc5d8f0e591d7039
Napppz <rizkytyan17@gmail.com>
2026-04-17 01:32:55 +0700

    Fix Admin

--- api/_lib/_db.js
+++ api/_lib/_db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('@neondatabase/serverless');
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_1yVLlBYH3qCM@ep-nameless-voice-ank083j1-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require';
+const connectionString = process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_7hRT9lBAXEaV@ep-rough-base-a19uwc5c-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
 
 const pool = new Pool({ connectionString });
 
